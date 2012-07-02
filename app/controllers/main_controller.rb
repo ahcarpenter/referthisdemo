3c1457dda3589549dcf4ee7eb96a1bd50f53c129
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-01 21:33:34 -0400

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,8 +1,8 @@
 class MainController < ApplicationController
   def index
     ReferThis.url(params[:referral], 1, request.base_url, 'Drew') if !params[:referral].nil?
-    @email_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['email']
-    @sms_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['sms']
-    @overall_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['overall']
+    @email_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['email'].to_s.truncate(8)
+    @sms_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['sms'].to_s.truncate(8)
+    @overall_clicked_through_rate = ReferThis.clicked_through_rate(true, true)['overall'].to_s.truncate(8)
   end
 end
